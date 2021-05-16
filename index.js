8833e962eb90df148ce0b62c0220513c777fd632
Hudson Pereira <hudson.byte@gmail.com>
2021-05-16 14:55:41 -0300

    ssl

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express()
-const port = 80
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/botruneterra.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/botruneterra.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/botruneterra.com/chain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
 
 app.use(express.static('assets'));
 
@@ -8,6 +21,14 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => {
-    console.log('website is up');
-})
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});

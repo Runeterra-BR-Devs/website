f0088588caabcf50aa4961d3a8d4d88b33d3721f
Hudson Pereira <hudson.byte@gmail.com>
2021-05-16 15:38:50 -0300

    adjustments for env awareness + favicon

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 
+const isProduction = process.env.NODE_ENV != 'development';
+
 // Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/botruneterra.com.br/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/botruneterra.com.br/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/botruneterra.com.br/chain.pem', 'utf8');
+const privateKey = isProduction ? fs.readFileSync('/etc/letsencrypt/live/botruneterra.com.br/privkey.pem', 'utf8') : '';
+const certificate = isProduction ? fs.readFileSync('/etc/letsencrypt/live/botruneterra.com.br/cert.pem', 'utf8') : '';
+const ca = isProduction ? fs.readFileSync('/etc/letsencrypt/live/botruneterra.com.br/chain.pem', 'utf8') : '';
 
 const credentials = {
     key: privateKey,
@@ -21,14 +23,20 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-// Starting both http & https servers
-http.createServer((req, res) => {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-}).listen(80);
+
+http.createServer(isProduction ? (req, res) => {
+    if (isProduction) {
+        res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+        return res.end();
+    }
+} : app).listen(80, () => {
+    console.log('HTTP Server running on port 80');
+});
 
 const httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(443, () => {
-	console.log('HTTPS Server running on port 443');
-});
+if (isProduction) {
+    httpsServer.listen(443, () => {
+        console.log('HTTPS Server running on port 443');
+    });
+}

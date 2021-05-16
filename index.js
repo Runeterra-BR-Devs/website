8d89f9e793f54930865d794744415faadca12bdf
Hudson Pereira <hudson.byte@gmail.com>
2021-05-16 15:06:14 -0300

    redirect to ssl

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ const credentials = {
 
 app.use(express.static('assets'));
 
+app.enable('trust proxy')
+app.use((req, res, next) => {
+    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+})
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
